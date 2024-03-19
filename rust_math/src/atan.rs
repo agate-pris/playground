4722b885aabed7d1b7ab5e91dc5a2e06abe515b1
agate-pris <agate-pris@users.noreply.github.com>
2024-03-19 22:01:00 +0900

    atan2_impl 簡略化

--- rust_math/src/atan.rs
+++ rust_math/src/atan.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 pub(crate) fn atan_impl<F>(x: i32, k: i32, f: F) -> i32
 where
     F: Fn(i32, i32) -> i32,
@@ -22,37 +20,30 @@ where
     if y == 0 && x == 0 {
         return 0;
     }
+
     let x_abs = (x as i64).abs();
     let y_abs = (y as i64).abs();
     let x_is_negative = x.is_negative();
     let y_is_negative = y.is_negative();
-    match x_abs.cmp(&y_abs) {
-        Ordering::Equal => match (x_is_negative, y_is_negative) {
-            (false, false) => k * k / 4,
-            (true, false) => k * k / 4 * 3,
-            (false, true) => k * k / -4,
-            (true, true) => k * k / 4 * -3,
-        },
-        Ordering::Greater => {
-            let x = (y_abs * k as i64 / x_abs) as i32;
-            let v = f(x);
-            match (x_is_negative, y_is_negative) {
-                (false, false) => v,
-                (true, false) => k * k - v,
-                (false, true) => -v,
-                (true, true) => v - k * k,
-            }
+
+    if x_abs > y_abs {
+        let x = (y_abs * k as i64 / x_abs) as i32;
+        let v = f(x);
+        match (x_is_negative, y_is_negative) {
+            (false, false) => v,
+            (true, false) => k * k - v,
+            (false, true) => -v,
+            (true, true) => v - k * k,
         }
-        Ordering::Less => {
-            let x = (x_abs * k as i64 / y_abs) as i32;
-            let v = f(x);
-            let right = k * k / 2;
-            match (x_is_negative, y_is_negative) {
-                (false, false) => right - v,
-                (true, false) => right + v,
-                (false, true) => -right + v,
-                (true, true) => -right - v,
-            }
+    } else {
+        let x = (x_abs * k as i64 / y_abs) as i32;
+        let v = f(x);
+        let right = k * k / 2;
+        match (x_is_negative, y_is_negative) {
+            (false, false) => right - v,
+            (true, false) => right + v,
+            (false, true) => -right + v,
+            (true, true) => -right - v,
         }
     }
 }
