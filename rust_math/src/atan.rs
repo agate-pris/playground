fc249b42e69e8f9d5905ad790e1824719fb77818
agate-pris <agate-pris@users.noreply.github.com>
2024-03-20 04:20:30 +0900

    atan2 共通テストを追加

--- rust_math/src/atan.rs
+++ rust_math/src/atan.rs
@@ -52,7 +52,11 @@ where
 mod tests {
     use std::f64::consts::PI;
 
-    use crate::{atan_p2::atan_p2_default, atan_p3::atan_p3_default, tests::read_data};
+    use crate::{
+        atan_p2::{atan2_p2_default, atan_p2_default},
+        atan_p3::{atan2_p3_default, atan_p3_default},
+        tests::read_data,
+    };
 
     fn test_atan<F>(f: F, data_path: &str, acceptable_error: f64)
     where
@@ -119,4 +123,112 @@ mod tests {
 
     #[rustfmt::skip] #[test] fn test_atan_p2() { test_atan(atan_p2_default, "data/atan_p2.json", 0.0039); }
     #[rustfmt::skip] #[test] fn test_atan_p3() { test_atan(atan_p3_default, "data/atan_p3.json", 0.0016); }
+
+    fn test_atan2_symmetry<F>(f: F, y_abs: i32, x_abs: i32, acceptable_error: f64)
+    where
+        F: Fn(i32, i32) -> i32,
+    {
+        #[rustfmt::skip]
+        let args = [
+           [ y_abs,  x_abs],
+           [ x_abs,  y_abs],
+           [ y_abs, -x_abs],
+           [ x_abs, -y_abs],
+           [-y_abs,  x_abs],
+           [-x_abs,  y_abs],
+           [-y_abs, -x_abs],
+           [-x_abs, -y_abs],
+        ];
+
+        const HALF_RIGHT: i32 = 2_i32.pow(i32::BITS / 2 - 1).pow(2) / 4;
+
+        let actuals = args.iter().map(|&[y, x]| f(y, x)).collect::<Vec<_>>();
+
+        if y_abs == 0 && x_abs == 0 {
+            assert!(actuals.iter().all(|&actual| actual == 0));
+        } else {
+            #[rustfmt::skip] assert_eq!( 2 * HALF_RIGHT - actuals[0], actuals[1]);
+            #[rustfmt::skip] assert_eq!( 4 * HALF_RIGHT - actuals[0], actuals[2]);
+            #[rustfmt::skip] assert_eq!( 2 * HALF_RIGHT + actuals[0], actuals[3]);
+            #[rustfmt::skip] assert_eq!(                 -actuals[0], actuals[4]);
+            #[rustfmt::skip] assert_eq!(-2 * HALF_RIGHT + actuals[0], actuals[5]);
+            #[rustfmt::skip] assert_eq!(-2 * HALF_RIGHT - actuals[0], actuals[7]);
+
+            if actuals[0] == 0 && y_abs == 0 {
+                assert_eq!(
+                    4 * HALF_RIGHT + actuals[0],
+                    actuals[6],
+                    "y_abs: {y_abs}, x_abs: {x_abs}"
+                );
+            } else {
+                assert_eq!(
+                    -4 * HALF_RIGHT + actuals[0],
+                    actuals[6],
+                    "y_abs: {y_abs}, x_abs: {x_abs}"
+                );
+            }
+        }
+
+        const TO_RAD: f64 = PI / 2_i32.pow(i32::BITS - 2) as f64;
+
+        let expected = args
+            .iter()
+            .map(|&[y, x]| (y as f64).atan2(x as f64))
+            .collect::<Vec<_>>();
+
+        actuals
+            .iter()
+            .zip(expected.iter())
+            .for_each(|(&actual, &expected)| {
+                approx::assert_abs_diff_eq!(
+                    actual as f64 * TO_RAD,
+                    expected,
+                    epsilon = acceptable_error
+                )
+            });
+    }
+
+    fn test_atan2<F>(f: F)
+    where
+        F: Fn(i32, i32) -> i32,
+    {
+        use std::i32::{MAX, MIN};
+
+        const EXP: u32 = i32::BITS / 2 - 1;
+        const K: i32 = 2_i32.pow(EXP);
+        const HALF_RIGHT: i32 = K.pow(2) / 4;
+
+        const ACCEPTABLE_ERROR: f64 = 0.000001;
+
+        test_atan2_symmetry(&f, 0, 0, ACCEPTABLE_ERROR);
+        test_atan2_symmetry(&f, 0, 1, ACCEPTABLE_ERROR);
+        test_atan2_symmetry(&f, 0, MAX, ACCEPTABLE_ERROR);
+        test_atan2_symmetry(&f, 1, 1, ACCEPTABLE_ERROR);
+        test_atan2_symmetry(&f, 1, MAX, ACCEPTABLE_ERROR);
+        test_atan2_symmetry(&f, MAX, MAX, ACCEPTABLE_ERROR);
+
+        assert_eq!(0, f(0, 1));
+        assert_eq!(0, f(0, MAX));
+        assert_eq!(HALF_RIGHT, f(1, 1));
+        assert_eq!(HALF_RIGHT, f(MAX, MAX));
+        assert_eq!(HALF_RIGHT * 4, f(0, MIN));
+        assert_eq!(HALF_RIGHT * -2, f(MIN, 0));
+        assert_eq!(HALF_RIGHT * -3, f(MIN, MIN));
+
+        let div_check = |a: i32, b: i32, c: i32, d: i32| {
+            assert_eq!(f(a, b), f(c, d));
+            test_atan2_symmetry(&f, a, b, 0.01);
+            test_atan2_symmetry(&f, c, d, 0.01);
+        };
+
+        #[rustfmt::skip] div_check( 9997, 32768,  3051, 10000); //  9997.51
+        #[rustfmt::skip] div_check(10000, 32768,  3052, 10000); // 10000.79
+        #[rustfmt::skip] div_check(10004, 32768,  3053, 10000); // 10004.07
+        #[rustfmt::skip] div_check( 9999, 32768, 15258, 50000); //  9999.48
+        #[rustfmt::skip] div_check(10000, 32768, 15259, 50000); // 10000.13
+        #[rustfmt::skip] div_check(10000, 32768, 15260, 50000); // 10000.79
+    }
+
+    #[rustfmt::skip] #[test] fn test_atan2_p2() { test_atan2(atan2_p2_default); }
+    #[rustfmt::skip] #[test] fn test_atan2_p3() { test_atan2(atan2_p3_default); }
 }
