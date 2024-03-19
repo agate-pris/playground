04c71d5ad0e381d6cebe3992fdb7f2463c0d09cb
agate-pris <agate-pris@users.noreply.github.com>
2024-03-20 03:32:33 +0900

    test_atan_p3 削除

--- rust_math/src/atan_p3.rs
+++ rust_math/src/atan_p3.rs
@@ -120,42 +120,6 @@ mod tests {
     }
 
     #[test]
-    fn test_atan_p3() {
-        const EXP: u32 = i32::BITS / 2 - 1;
-        const K: i32 = 2_i32.pow(EXP);
-
-        fn f(x: i32) -> f64 {
-            let (a, b) = calc_default_p3_k(EXP);
-            let expected = (x as f64 / K as f64).atan();
-            let actual = {
-                let actual = atan_p3(x, K, a, b);
-                actual as f64 * PI / 2.0_f64.powi(2 * EXP as i32)
-            };
-            let error = actual - expected;
-            assert!(
-                error.abs() < 0.0016,
-                "error: {error}, x: {x}, expected: {expected}, actual: {actual}"
-            );
-            error
-        }
-
-        f(0);
-        f(i32::MAX);
-        f(i32::MIN);
-
-        let mut min = std::f64::INFINITY;
-        let mut max = std::f64::NEG_INFINITY;
-        for degrees in -44..45 {
-            let x = (degrees as f64).to_radians().tan();
-            let x = (x * K as f64).round_ties_even() as i32;
-            let e = f(x);
-            min = min.min(e);
-            max = max.max(e);
-        }
-        println!("min: {min}, max: {max}");
-    }
-
-    #[test]
     fn test_atan_p3_default() {
         const EXP: u32 = i32::BITS / 2 - 1;
         const K: i32 = 2_i32.pow(EXP);
