c35299dbab6e78102d27804ba1758d3224a71141
agate-pris <agate-pris@users.noreply.github.com>
2024-03-25 02:19:58 +0900

    test_atan 改善

--- rust_math/src/atan.rs
+++ rust_math/src/atan.rs
@@ -50,7 +50,10 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::f64::consts::PI;
+    use std::{
+        f64::consts::PI,
+        f64::{INFINITY, NEG_INFINITY},
+    };
 
     use approx::assert_abs_diff_eq;
 
@@ -69,64 +72,54 @@ mod tests {
 
         const EXP: u32 = i32::BITS / 2 - 1;
         const K: i32 = 2_i32.pow(EXP);
-        const HALF_RIGHT: i32 = K.pow(2) / 4;
+        const K_2: i32 = K.pow(2);
+        const RIGHT: i32 = K_2 / 2;
+        const HALF_RIGHT: i32 = RIGHT / 2;
 
         let expected = read_data::<i32>(data_path).unwrap();
 
+        assert_eq!(expected.len(), (K + 1) as usize);
         assert_eq!(expected[0], 0);
         assert_eq!(expected[K as usize], HALF_RIGHT);
 
-        assert_eq!(f(0), 0);
-        assert_eq!(f(K), HALF_RIGHT);
-        assert_eq!(f(-K), -HALF_RIGHT);
+        let mut min = INFINITY;
+        let mut max = NEG_INFINITY;
 
-        let f_max = f(MAX);
-        let f_min = f(MIN);
-
-        assert_eq!(f_max, 2 * HALF_RIGHT);
-        assert_eq!(f_min, -2 * HALF_RIGHT);
-        assert_eq!(f_max, f(K * K + 1));
-        assert_eq!(f_min, f(-K * K - 1));
-
-        for i in 1..=K {
-            const RIGHT: i32 = 2 * HALF_RIGHT;
-
-            let expected = expected[i as usize];
-            let inv_i = K.pow(2) / i;
-
-            let fi = f(i);
-
-            assert_eq!(fi, expected);
-            assert_eq!(f(-i), -expected);
-            assert_eq!(f(inv_i), RIGHT - expected);
-            assert_eq!(f(-inv_i), expected - RIGHT);
-
-            assert_abs_diff_eq!(
-                fi as f64 * PI / K.pow(2) as f64,
-                (i as f64 / K as f64).atan(),
-                epsilon = acceptable_error
-            );
-        }
-
-        // bignum
-        {
-            // 17th mersenne prime
-            const STEP: usize = 131071;
-
-            for x in (MIN..=MAX).step_by(STEP) {
-                if (-K..=K).contains(&x) {
-                    continue;
-                }
-                let actual = f(x);
-                let expected = x.signum() * (K * K / 2 - f((K * K / x).abs()));
-                assert_eq!(expected, actual);
+        let mut verify = |x: i32, expected: i32, neg: bool| {
+            let mut f = |x: i32, expected: i32| {
+                assert_eq!(f(x), expected);
+                let actual = expected as f64 * PI / K_2 as f64;
+                let expected = (x as f64 / K as f64).atan();
+                assert_abs_diff_eq!(expected, actual, epsilon = acceptable_error);
+                min = min.min(actual - expected);
+                max = max.max(actual - expected);
+            };
+            f(x, expected);
+            if neg {
+                f(-x, -expected);
             }
+        };
+
+        verify(0, 0, false);
+        verify(MAX, RIGHT, false);
+        verify(MIN, -RIGHT, false);
+        verify(K_2 + 1, RIGHT, true);
+        verify(K, HALF_RIGHT, true);
+
+        for i in 1..K {
+            let expected = expected[i as usize];
+            let inv = RIGHT - expected;
+            verify(i, expected, true);
+            verify(K * K / i, inv, true);
+            verify(K * K / (i + 1) + 1, inv, true);
         }
+
+        println!("min: {min}, max: {max}");
     }
 
-    #[rustfmt::skip] #[test] fn test_atan_p2() { test_atan(atan_p2_default, "data/atan_p2.json", 0.0039);  }
-    #[rustfmt::skip] #[test] fn test_atan_p3() { test_atan(atan_p3_default, "data/atan_p3.json", 0.0016);  }
-    #[rustfmt::skip] #[test] fn test_atan_p5() { test_atan(atan_p5_default, "data/atan_p5.json", 0.00092); }
+    #[rustfmt::skip] #[test] fn test_atan_p2() { test_atan(atan_p2_default, "data/atan_p2.json", 0.003852); }
+    #[rustfmt::skip] #[test] fn test_atan_p3() { test_atan(atan_p3_default, "data/atan_p3.json", 0.001601); }
+    #[rustfmt::skip] #[test] fn test_atan_p5() { test_atan(atan_p5_default, "data/atan_p5.json", 0.000922); }
 
     fn test_atan2_symmetry<F>(f: F, y_abs: i32, x_abs: i32, acceptable_error: f64)
     where
