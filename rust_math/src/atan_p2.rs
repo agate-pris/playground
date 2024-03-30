1357b82c479bf96dc70c4b9250ec3fd482392e6d
agate-pris <agate-pris@users.noreply.github.com>
2024-03-30 13:38:52 +0900

    AtanP2Default を実装

--- rust_math/src/atan_p2.rs
+++ rust_math/src/atan_p2.rs
@@ -1,5 +1,13 @@
 use std::f64::consts::PI;
 
+use fixed::{
+    traits::Fixed,
+    types::{
+        I10F22, I10F6, I11F21, I11F5, I12F20, I12F4, I13F19, I13F3, I14F18, I15F17, I16F16, I17F15,
+        I18F14, I19F13, I20F12, I21F11, I22F10, I23F9, I24F8, I25F7, I26F6, I2F6, I3F5, I6F10,
+        I6F26, I7F25, I7F9, I8F24, I8F8, I9F23, I9F7,
+    },
+};
 use num_traits::{AsPrimitive, ConstZero, NumOps, PrimInt, Signed};
 use primitive_promotion::PrimitivePromotionExt;
 
@@ -12,6 +20,25 @@ pub trait AtanP2Default {
     type Bits;
     const A: Self::Bits;
 }
+
+macro_rules! impl_atan_p2_default_fixed {
+    ($($t:ty, $a:expr),*) => {
+        $(
+            impl AtanP2Default for $t {
+                type Bits = <Self as Fixed>::Bits;
+                const A: Self::Bits = $a;
+            }
+        )*
+    };
+}
+
+impl_atan_p2_default_fixed!(
+    I3F5, 0, I2F6, 0, I13F3, 179, I12F4, 91, I11F5, 47, I10F6, 24, I9F7, 13, I8F8, 8, I7F9, 5,
+    I6F10, 3, I26F6, 1458371, I25F7, 729188, I24F8, 364590, I23F9, 182295, I22F10, 91148, I21F11,
+    45575, I20F12, 22789, I19F13, 11395, I18F14, 5699, I17F15, 2850, I16F16, 1426, I15F17, 714,
+    I14F18, 358, I13F19, 180, I12F20, 91, I11F21, 47, I10F22, 25, I9F23, 14, I8F24, 8, I7F25, 5,
+    I6F26, 3
+);
 
 /// ```rust
 /// use rust_math::atan_p2::*;
