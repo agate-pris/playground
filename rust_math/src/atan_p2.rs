6190c752dd1d12c2e582529f488ec70998f54243
agate-pris <agate-pris@users.noreply.github.com>
2024-04-22 12:46:42 +0900

    AtanP2::atan_p2 逆数を求める際に四捨五入を行うように修正

--- rust_math/src/atan_p2.rs
+++ rust_math/src/atan_p2.rs
@@ -2,6 +2,8 @@ use std::cmp::Ordering;
 
 use num_traits::{ConstZero, Signed};
 
+use crate::atan::inv_i32_f15;
+
 fn atan_p2_impl<T>(x: T, one: T, frac_k_4: T, a: T) -> T
 where
     T: Copy + Signed,
@@ -37,19 +39,14 @@ impl AtanP2 for i32 {
     type Output = i32;
 
     fn atan_p2(self) -> Self::Output {
-        fn inv(x: i32) -> i32 {
-            const K: i32 = 2_i32.pow(i32::BITS - 2);
-            K / x
-        }
-
         const RIGHT: i32 = 2_i32.pow(i32::BITS - 3);
         const NEG_ONE: i32 = -<i32 as AtanP2Consts<i32>>::ONE;
 
         if self < NEG_ONE {
             const NEG_RIGHT: i32 = -RIGHT;
-            NEG_RIGHT - i32::calc(inv(self))
+            NEG_RIGHT - i32::calc(inv_i32_f15(self))
         } else if self > <i32 as AtanP2Consts<i32>>::ONE {
-            RIGHT - i32::calc(inv(self))
+            RIGHT - i32::calc(inv_i32_f15(self))
         } else {
             i32::calc(self)
         }
