bc982cdd3db6b1fa1b75b948b12712cbd1e7c46f
agate-pris <agate-pris@users.noreply.github.com>
2024-05-02 01:06:33 +0900

    atan_p3 定数の定義を修正

--- rust_math/src/atan_p3.rs
+++ rust_math/src/atan_p3.rs
@@ -9,6 +9,40 @@ macro_rules! atan_p3_impl {
     }};
 }
 
+pub const A_B_I8: [(i8, i8); 2] = [(0, 0), (0, 0)];
+
+pub const A_B_I16: [(i16, i16); 8] = [
+    (2, 2),
+    (2, 5),
+    (4, 7),
+    (9, 8),
+    (20, 9),
+    (41, 12),
+    (80, 26),
+    (159, 46),
+];
+
+pub const A_B_I32: [(i32, i32); 18] = [
+    (2, 2),
+    (2, 5),
+    (4, 7),
+    (6, 13),
+    (18, 13),
+    (38, 18),
+    (79, 26),
+    (162, 42),
+    (322, 85),
+    (640, 173),
+    (1279, 344),
+    (2555, 691),
+    (5107, 1383),
+    (10212, 2766),
+    (20421, 5534),
+    (40841, 11064),
+    (81678, 22133),
+    (163355, 44265),
+];
+
 pub trait AtanP3Consts<T> {
     const ONE: T;
     const FRAC_K_4: T;
@@ -27,8 +61,8 @@ struct AtanP3ConstsI32();
 impl AtanP3Consts<i32> for AtanP3ConstsI32 {
     const ONE: i32 = 2_i32.pow(i32::BITS / 2 - 1);
     const FRAC_K_4: i32 = 2_i32.pow(i32::BITS / 2 - 3);
-    const A: i32 = 2555;
-    const B: i32 = 691;
+    const A: i32 = A_B_I32[11].0;
+    const B: i32 = A_B_I32[11].1;
 }
 
 struct AtanP3I32Util();
@@ -69,16 +103,6 @@ impl AtanP3 for i32 {
     }
 }
 
-/*
-impl_atan_p3_default_fixed!(
-    I3F5, 0, 0, I2F6, 0, 0, I13F3, 159, 46, I12F4, 80, 26, I11F5, 41, 12, I10F6, 20, 9, I9F7, 9, 8,
-    I8F8, 4, 7, I7F9, 2, 5, I6F10, 2, 2, I23F9, 163355, 44265, I22F10, 81678, 22133, I21F11, 40841,
-    11064, I20F12, 20421, 5534, I19F13, 10212, 2766, I18F14, 5107, 1383, I17F15, 2555, 691, I16F16,
-    1279, 344, I15F17, 640, 173, I14F18, 322, 85, I13F19, 162, 42, I12F20, 79, 26, I11F21, 38, 18,
-    I10F22, 18, 13, I9F23, 6, 13, I8F24, 4, 7, I7F25, 2, 5, I6F26, 2, 2
-);
-*/
-
 #[cfg(test)]
 mod tests {
     use std::{
@@ -92,7 +116,6 @@ mod tests {
     use primitive_promotion::PrimitivePromotionExt;
     use rand::prelude::*;
     use rayon::prelude::*;
-    use rstest::rstest;
 
     use crate::{atan::tests::compare_error, bits::Bits};
 
@@ -173,32 +196,24 @@ mod tests {
         assert_eq!(expected, k);
     }
 
-    #[rstest]
-    #[case(2, vec![(3, 0), (3, 1)])]
-    #[case(3, vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)])]
-    #[case(4, vec![(0, 0), (0, 1), (1, 0), (1, 1)])]
-    //#[case(5, vec![(I3F5::A, I3F5::B)])]
-    //#[case(6, vec![(I2F6::A, I2F6::B)])]
-    fn test_optimal_constants_i8(#[case] exp: u32, #[case] expected: Vec<(i8, i8)>) {
-        test_optimal_constants(exp, expected);
+    #[test]
+    fn test_optimal_constants_i8() {
+        for (i, &(a, b)) in A_B_I8.iter().enumerate() {
+            test_optimal_constants(6 - i as u32, vec![(a, b)]);
+        }
+        let expected: Vec<(i8, i8)> = vec![(0, 0), (0, 1), (1, 0), (1, 1)];
+        test_optimal_constants(4, expected);
     }
 
-    #[rstest]
-    #[case(2, vec![(323, 86), (324, 84), (324, 85), (325, 82), (325, 83)])]
-    //#[case(3, vec![(I13F3::A, I13F3::B)])]
-    //#[case(4, vec![(I12F4::A, I12F4::B)])]
-    //#[case(5, vec![(I11F5::A, I11F5::B)])]
-    //#[case(6, vec![(I10F6::A, I10F6::B)])]
-    //#[case(7, vec![(I9F7::A, I9F7::B)])]
-    //#[case(8, vec![(I8F8::A, I8F8::B)])]
-    //#[case(9, vec![(I7F9::A, I7F9::B)])]
-    //#[case(10, vec![(I6F10::A, I6F10::B)])]
-    #[case(11, vec! [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)])]
-    #[case(12, vec! [(0, 0), (0, 1), (1, 0), (1, 1)])]
-    #[case(13, vec![(0, 0)])]
-    #[case(14, vec![(0, 0)])]
-    fn test_optimal_constants_i16(#[case] exp: u32, #[case] expected: Vec<(i16, i16)>) {
-        test_optimal_constants(exp, expected);
+    #[test]
+    fn test_optimal_constants_i16() {
+        let expected: Vec<(i16, i16)> = vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)];
+        test_optimal_constants(11, expected);
+        for (i, &(a, b)) in A_B_I16.iter().enumerate() {
+            test_optimal_constants(10 - i as u32, vec![(a, b)]);
+        }
+        let expected: Vec<(i16, i16)> = vec![(323, 86), (324, 84), (324, 85), (325, 82), (325, 83)];
+        test_optimal_constants(2, expected);
     }
 
     // Test as `cargo test -- atan_p3::tests::test_optimal_constants --ignored --nocapture --test-threads=1`
@@ -213,27 +228,24 @@ mod tests {
                 }
             };
         }
-        define_test!(case_01, 30, vec![(0, 0)]);
-        define_test!(case_02, 29, vec![(0, 0)]);
-        define_test!(case_03, 28, vec![(0, 0), (0, 1), (1, 0), (1, 1)]);
-        define_test!(case_04, 27, vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)]);
-        //define_test!(case_05, 26, vec![(I6F26::A, I6F26::B)]);
-        //define_test!(case_06, 25, vec![(I7F25::A, I7F25::B)]);
-        //define_test!(case_07, 24, vec![(I8F24::A, I8F24::B)]);
-        //define_test!(case_08, 23, vec![(I9F23::A, I9F23::B)]);
-        //define_test!(case_09, 22, vec![(I10F22::A, I10F22::B)]);
-        //define_test!(case_10, 21, vec![(I11F21::A, I11F21::B)]);
-        //define_test!(case_11, 20, vec![(I12F20::A, I12F20::B)]);
-        //define_test!(case_12, 19, vec![(I13F19::A, I13F19::B)]);
-        //define_test!(case_13, 18, vec![(I14F18::A, I14F18::B)]);
-        //define_test!(case_14, 17, vec![(I15F17::A, I15F17::B)]);
-        //define_test!(case_15, 16, vec![(I16F16::A, I16F16::B)]);
-        define_test!(case_16, 15, vec![(AtanP3ConstsI32::A, AtanP3ConstsI32::B)]);
-        //define_test!(case_17, 14, vec![(I18F14::A, I18F14::B)]);
-        //define_test!(case_18, 13, vec![(I19F13::A, I19F13::B)]);
-        //define_test!(case_19, 12, vec![(I20F12::A, I20F12::B)]);
-        //define_test!(case_20, 11, vec![(I21F11::A, I21F11::B)]);
-        //define_test!(case_21, 10, vec![(I22F10::A, I22F10::B)]);
-        //define_test!(case_22, 9, vec![(I23F9::A, I23F9::B)]);
+        define_test!(case_27, 27, vec![(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)]);
+        define_test!(case_26, 26, vec![(A_B_I32[0])]);
+        define_test!(case_25, 25, vec![(A_B_I32[1])]);
+        define_test!(case_24, 24, vec![(A_B_I32[2])]);
+        define_test!(case_23, 23, vec![(A_B_I32[3])]);
+        define_test!(case_22, 22, vec![(A_B_I32[4])]);
+        define_test!(case_21, 21, vec![(A_B_I32[5])]);
+        define_test!(case_20, 20, vec![(A_B_I32[6])]);
+        define_test!(case_19, 19, vec![(A_B_I32[7])]);
+        define_test!(case_18, 18, vec![(A_B_I32[8])]);
+        define_test!(case_17, 17, vec![(A_B_I32[9])]);
+        define_test!(case_16, 16, vec![(A_B_I32[10])]);
+        define_test!(case_15, 15, vec![(A_B_I32[11])]);
+        define_test!(case_14, 14, vec![(A_B_I32[12])]);
+        define_test!(case_13, 13, vec![(A_B_I32[13])]);
+        define_test!(case_12, 12, vec![(A_B_I32[14])]);
+        define_test!(case_11, 11, vec![(A_B_I32[15])]);
+        define_test!(case_10, 10, vec![(A_B_I32[16])]);
+        define_test!(case_09, 9, vec![(A_B_I32[17])]);
     }
 }
