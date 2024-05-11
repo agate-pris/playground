d5e793c79ad0d83b173174f39f1b7a7439f2acd6
agate-pris <agate-pris@users.noreply.github.com>
2024-05-12 00:54:14 +0900

    sin_cos を sin に改名

--- rust_math/src/lib.rs
+++ rust_math/src/lib.rs
@@ -10,13 +10,13 @@ mod atan_p2;
 mod atan_p3;
 mod atan_p5;
 mod bits;
-mod sin_cos;
+mod sin;
 
 pub use atan_p2::{atan2_p2_2850, atan_p2_2850};
 pub use atan_p3::{atan2_p3_2555_691, atan_p3_2555_691};
 pub use atan_p5::{atan2_p5_787_2968, atan_p5_787_2968};
 pub use bits::Bits;
-pub use sin_cos::{
+pub use sin::{
     cos_p2_i32, cos_p3_16384, cos_p4_7032, cos_p4_7384, cos_p5_51437, cos_p5_51472, sin_p2_i32,
     sin_p3_16384, sin_p4_7032, sin_p4_7384, sin_p5_51437, sin_p5_51472,
 };
