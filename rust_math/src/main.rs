74a3e01b98678d6298c9b1f298a67d4971c7d24d
agate-pris <agate-pris@users.noreply.github.com>
2024-05-12 00:02:01 +0900

    新しいクレートに移動した機能を元のクレートから削除

--- rust_math/src/main.rs
+++ rust_math/src/main.rs
@@ -1,17 +1,8 @@
 use clap::Parser;
-use rust_math::{
-    atan_p2_2850, atan_p3_2555_691, atan_p5_787_2968, cos_p2_i32, cos_p4_7032, cos_p4_7384,
-    sin_p3_16384, sin_p5_51437, sin_p5_51472,
-};
+use rust_math::{atan_p2_2850, atan_p3_2555_691, atan_p5_787_2968};
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[rustfmt::skip] #[arg(long)] cos_p2: bool,
-    #[rustfmt::skip] #[arg(long)] sin_p3: bool,
-    #[rustfmt::skip] #[arg(long)] cos_p4: bool,
-    #[rustfmt::skip] #[arg(long)] sin_p5: bool,
-    #[rustfmt::skip] #[arg(long)] cos_p4o: bool,
-    #[rustfmt::skip] #[arg(long)] sin_p5o: bool,
     #[rustfmt::skip] #[arg(long)] atan_p2: bool,
     #[rustfmt::skip] #[arg(long)] atan_p3: bool,
     #[rustfmt::skip] #[arg(long)] atan_p5: bool,
@@ -33,24 +24,6 @@ fn main() {
         print!("]");
     }
 
-    if args.cos_p2 {
-        print(cos_p2_i32);
-    }
-    if args.sin_p3 {
-        print(sin_p3_16384);
-    }
-    if args.cos_p4 {
-        print(cos_p4_7032);
-    }
-    if args.sin_p5 {
-        print(sin_p5_51472);
-    }
-    if args.cos_p4o {
-        print(cos_p4_7384);
-    }
-    if args.sin_p5o {
-        print(sin_p5_51437);
-    }
     if args.atan_p2 {
         print(atan_p2_2850);
     }
